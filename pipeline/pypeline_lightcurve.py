553f2e2da284805c5372bc1bcbc4f9fc683243c6
natto <natto@natto-tp.none>
2015-04-12 02:32:48 -0400

    add option to run process pds3 24 color data

--- pipeline/pypeline_lightcurve.py
+++ pipeline/pypeline_lightcurve.py
@@ -1,8 +1,17 @@
+'''
+        example call: python pypeline_lightcurve.py -p ALCDEF
+        example call: python pypeline_lightcurve.py -p EAR_A_DBP_3_RDR_24COLOR_V2_1
+
+        runs workflow on the dir... generates csv
+'''
 from common import *
 from lib.parse_ALCDEF import parse_file as parse_ALCDEF_file
+from lib.parse_24color import parse_label_string as parse_pds3_24color_label_string 
+from os.path import split as psplit
 
 import pandas as pd
 import json
+from StringIO import StringIO
 
 class ALCDEFOutputSpec(TraitedSpec):
     csv_filepath = traits.File()
@@ -36,6 +45,34 @@ class ALCDEFConversionTask(BaseInterface):
                 
         return runtime
 
+class PDS324ColorConversionTask(BaseInterface):
+    input_spec = SimpleFileInputSpec
+    output_spec = ALCDEFOutputSpec
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        for trait_name in outputs.keys():
+            outputs[trait_name] = getattr(self, '_%s'%trait_name)
+        return outputs
+    
+    def _run_interface(self, runtime):
+        dlbl = parse_pds3_24color_label_string(open(self.inputs.filepath).read())
+        
+        tab_filepath = pjoin(psplit(self.inputs.filepath)[0], dlbl['^TABLE'].strip('"'))
+        sio = StringIO(open(tab_filepath).read())
+        tab = pd.read_table(sio, sep='\s+', header=None)
+        sio.close()
+        
+        ncol = int(dlbl['TABLE'][0]['COLUMNS'])
+        tab.columns = [col['NAME'].strip('"') for col in dlbl['TABLE'][0]['COLUMN']]
+        
+        self._json_filepath = os.path.join(os.getcwd(), 'output.json')
+        # with open(self._json_filepath, 'w') as ofile:
+        #     ofile.write(json.dumps(out))
+        
+        self._csv_filepath = os.path.join(os.getcwd(), 'output.csv')
+        tab.to_csv(self._csv_filepath, index=False)
+        return runtime
 
 if __name__ == '__main__':
 
@@ -44,72 +81,88 @@ if __name__ == '__main__':
     import os
     from os.path import join as pjoin
     
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='the lightcurve zipfile or its extracted directory')
-    args = parser.parse_args()
-
-    if not args.input or not os.path.exists(args.input):
-        parser.print_help()
-
-        print('''
-        example call: python %s -d path/to/ALCDEF
-
-        runs workflow on the dir
-        '''% __file__)
-        sys.exit()
-
-    if os.path.isdir(args.input):
-        file_list = glob(pjoin(args.input, '*.txt'))[:10]
-    elif args.input.endswith('.zip'):
-        import tempfile
-        import zipfile
-        import tempfile
-        import os
-        from os.path import join as pjoin
-
-        output_dir = tempfile.mkdtemp()
-        print('zip file given. inflating to %s...' % (output_dir))
-
-        zf = zipfile.ZipFile(args.input, 'r')
-        file_list = []
-        for info in zf.infolist():
-            output_filepath = output_dir + info.filename
-            with open(output_filepath, 'wb') as ofile:
-                ofile.write(zf.read(info.filename))
-            file_list.append(output_filepath)
-    
-    print('{} files found.'.format(len(file_list)))
-
-    nInputFileList = pe.Node(interface = util.IdentityInterface(fields = ['input_filepath']), name = 'input_file_list')
-    nInputFileList.iterables = ('input_filepath', file_list)
-    
-    nALCDEFConversion = pe.Node(interface = ALCDEFConversionTask(), name='alcdef_conversion')
+    # preprocessing: read various lightcurve data, output unified structures (csv)
+    wf_preproc = pe.Workflow(name='lightcurve_workflow')
+    wf_preproc.config['execution']['crashdump_dir'] = 'crashdump'
+    # change me
+    wf_preproc.base_dir = '/tmp/wf-asteroid'
     
     nDataSink = pe.Node(interface=nio.DataSink(parameterization=False), name='datasink')
     nDataSink.inputs.base_directory = '/tmp/asteroid-datasink'
-    
-    wf = pe.Workflow(name='lightcurve_workflow')
-    wf.config['execution']['crashdump_dir'] = 'crashdump'
-    # change me
-    wf.base_dir = '/tmp/wf-asteroid'
-    
-    # substitutions for datasink
-    def make_substitution(input_filepath):
-        import os.path as p
-        filename_root = p.splitext(p.split(input_filepath)[1])[0]
-        return [("output", filename_root)]
 
-    wf.connect([
-        (nInputFileList, nALCDEFConversion, [('input_filepath', 'filepath')]),
-        (nALCDEFConversion, NodePrinter.create(), [('json_filepath', 'input')]),
-        (nALCDEFConversion, NodePrinter.create(), [('csv_filepath', 'input')]),
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--alcdef', help='the ALCDEF zipfile or its extracted directory')
+    parser.add_argument('-p', '--pds3_24color', help='PDS3 24 color extracted directory')
+    args = parser.parse_args()
+    
+    if args.alcdef:
+        if os.path.isdir(args.alcdef):
+            file_list = glob(pjoin(args.input, '*.txt'))[:10]
+        elif args.input.endswith('.zip'):
+            import tempfile
+            import zipfile
+            import tempfile
+            import os
+            from os.path import join as pjoin
 
-        (nInputFileList, nDataSink, [(("input_filepath", make_substitution), "substitutions")]),
-        
-        (nALCDEFConversion, nDataSink, [('json_filepath', 'json')]),
-        (nALCDEFConversion, nDataSink, [('csv_filepath', 'csv')]),
+            output_dir = tempfile.mkdtemp()
+            print('zip file given. inflating to %s...' % (output_dir))
+
+            zf = zipfile.ZipFile(args.input, 'r')
+            file_list = []
+            for info in zf.infolist():
+                output_filepath = output_dir + info.filename
+                with open(output_filepath, 'wb') as ofile:
+                    ofile.write(zf.read(info.filename))
+                file_list.append(output_filepath)
+    
+        print('{} files found.'.format(len(file_list)))
+
+        nInputFileList = pe.Node(interface = util.IdentityInterface(fields = ['input_filepath']), name = 'input_file_list')
+        nInputFileList.iterables = ('input_filepath', file_list)
+
+        nALCDEFConversion = pe.Node(interface = ALCDEFConversionTask(), name='alcdef_conversion')
+
+        # substitutions for datasink
+        def make_substitution(input_filepath):
+            import os.path as p
+            filename_root = p.splitext(p.split(input_filepath)[1])[0]
+            return [("output", p.join('ALCDEF', filename_root))]
+
+        wf_preproc.connect([
+            (nInputFileList, nALCDEFConversion, [('input_filepath', 'filepath')]),
+            (nInputFileList, nDataSink, [(("input_filepath", make_substitution), "substitutions")]),
+
+            # (nALCDEFConversion, nDataSink, [('json_filepath', 'json')]),
+            (nALCDEFConversion, nDataSink, [('csv_filepath', 'csv')]),
+            ])
+    elif args.pds3_24color:
+
+        file_list = glob(pjoin(args.pds3_24color, 'data', 'data4', '*.lbl'))
+        file_list = file_list[:3]
+        nInputFileList = pe.Node(interface = util.IdentityInterface(fields = ['input_filepath']), name = 'input_file_list')
+        nInputFileList.iterables = ('input_filepath', file_list)
+
+        nPDS324ColorConversionTask = pe.Node(interface = PDS324ColorConversionTask(), name='pds3_24color_conversion')
+
+        # substitutions for datasink
+        def make_substitution(input_filepath):
+            import os.path as p
+            filename_root = p.splitext(p.split(input_filepath)[1])[0]
+            return [('output', p.join('PDS3_24COLOR', filename_root))]
+        wf_preproc.connect([
+            (nInputFileList, nPDS324ColorConversionTask, [('input_filepath', 'filepath')]),
+            (nInputFileList, nDataSink, [(("input_filepath", make_substitution), "substitutions")]),
+
+            (nPDS324ColorConversionTask, nDataSink, [('csv_filepath', 'csv')]),
+            ])
+
+
+    wf_preproc.connect([
+        (nInputFileList, NodePrinter.create(), [('input_filepath', 'input')]),
         ])
-    wf.write_graph()
-    res = wf.run()
+
+    wf_preproc.write_graph()
+    res = wf_preproc.run()
 
 
